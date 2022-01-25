a9d02329d3c958d5c4c3034027031d95b13fdfed
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2022-01-25 23:01:04 +0800

    Create diy-part1.sh

--- beta/diy-part1.sh
+++ beta/diy-part1.sh
@@ -6,5 +6,5 @@ sed -i 's/PATCHVER=5.4/PATCHVER=5.10/g' target/linux/rockchip/Makefile
 
 
 ## R2C WAN口退回之前的驱动
-git reset b1b36c86af6d9fa3ba57eb2140025d2039996b81 target/linux/rockchip/patches-5.4/600-net-phy-Add-driver-for-Motorcomm-YT85xx-PHYs.patch
-git checkout target/linux/rockchip/patches-5.4/600-net-phy-Add-driver-for-Motorcomm-YT85xx-PHYs.patch
+# git reset b1b36c86af6d9fa3ba57eb2140025d2039996b81 target/linux/rockchip/patches-5.4/600-net-phy-Add-driver-for-Motorcomm-YT85xx-PHYs.patch
+# git checkout target/linux/rockchip/patches-5.4/600-net-phy-Add-driver-for-Motorcomm-YT85xx-PHYs.patch
