c7524c0f1594fa85eedbb16d1cc004efdb3eaa25
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2022-05-06 12:11:49 +0800

    Create diy-part1.sh

--- beta/diy-part1.sh
+++ beta/diy-part1.sh
@@ -6,8 +6,8 @@
 
 
 # 交换 LAN/WAN 口
-sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
-sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+#sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+#sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
 
 
