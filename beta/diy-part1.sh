5968bacafcfa794d6ec4856aff3ad9d52bd71aa8
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2022-05-24 09:43:10 +0800

    Update diy-part1.sh

--- beta/diy-part1.sh
+++ beta/diy-part1.sh
@@ -18,10 +18,10 @@
 
 
 ## 替换rtl8821cu驱动
-# rm -rf package/kernel/rtl8821cu
-# git clone https://github.com/brektrou/rtl8821CU.git package/kernel/rtl8821cu
+rm -rf package/kernel/rtl8821cu
+git clone https://github.com/brektrou/rtl8821CU.git package/kernel/rtl8821cu
 
 # lede 20210924版本
 # git reset 8eaa4051c98abf6a7f8594b165cea862ef21a972 package/kernel/rtl8821cu
 
-# git checkout package/kernel/rtl8821cu
+git checkout package/kernel/rtl8821cu
